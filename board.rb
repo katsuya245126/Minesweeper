d190529e9af1b01566bfdce166ca9ef4950dff1e
Sung Guk Lee <katsuya245126@gmail.com>
2020-06-24 16:47:45 +0900

    Change board making method to class method and write place bomb method

--- board.rb
+++ board.rb
@@ -1,5 +1,33 @@
+require_relative 'tile.rb'
+
 class Board
+  def self.empty_board
+    board = Array.new(9) { [] }
+    board.each do |row|
+      9.times do
+        row << Tile.new(false)
+      end
+    end
+    board
+  end
+
   def initialize
-    @board = Array.new(9) { Array.new(9) }
+    @board = place_bomb(Board.empty_board)
+  end
+
+  def place_bomb(board, num = 9)
+    remaining_bombs = num
+    bombed_board = board
+
+    until remaining_bombs.zero?
+      y = rand(0..8)
+      x = rand(0..8)
+      next if board[y][x].bomb?
+
+      board[y][x] = Tile.new(true)
+      remaining_bombs -= 1
+    end
+
+    bombed_board
   end
 end
