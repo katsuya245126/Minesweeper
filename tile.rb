82aab7f4191bc2df3be0c8b7b4b9a5c4046a1028
Sung Guk Lee <katsuya245126@gmail.com>
2020-06-24 16:26:48 +0900

    Add getters and setters for tile class

--- tile.rb
+++ tile.rb
@@ -1,3 +1,27 @@
 class Tile
+  def initialize(bomb)
+    @bomb = bomb
+    @flagged = false
+    @revealed = false
+  end
 
+  def bombed?
+    bomb
+  end
+
+  def flagged?
+    flagged
+  end
+
+  def revealed?
+    revealed
+  end
+
+  def reveal
+    @revealed = true
+  end
+
+  private
+
+  attr_reader :bomb, :flagged, :revealed
 end
