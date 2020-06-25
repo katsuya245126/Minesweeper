fad6ca812e363e8573bba19fc737681ae618907b
Sung Guk Lee <katsuya245126@gmail.com>
2020-06-25 13:53:39 +0900

    Moved methods to count neighbor bombs to board

--- board.rb
+++ board.rb
@@ -1,6 +1,19 @@
 require_relative 'tile.rb'
 
 class Board
+  NEIGHBOR_POS = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1]
+  ].freeze
+
+  attr_reader :board
+
   def self.empty_board
     board = Array.new(9) { [] }
     board.each do |row|
@@ -13,6 +26,50 @@ class Board
 
   def initialize
     @board = place_bomb(Board.empty_board)
+  end
+
+  def get_tile(pos)
+    y, x = pos
+    board[y][x]
+  end
+
+  def render
+    puts
+    puts "  #{(0..8).to_a.join('  ')}"
+    board.each_with_index do |row, idx|
+      print idx.to_s
+      row.each do |tile|
+        print " #{tile} "
+      end
+      puts
+    end
+    puts
+  end
+
+  def reveal(pos)
+    nil
+  end
+
+  private
+
+  def neighbor_bomb_count(my_pos)
+    neighbor_idx = get_neighbor_idx(my_pos)
+    bomb_count = 0
+
+    neighbor_idx.each do |pos|
+      y, x = pos
+      bomb_count += 1 if board[y][x].bomb?
+    end
+
+    bomb_count
+  end
+
+  def get_neighbor_idx(my_pos)
+    NEIGHBOR_POS.dup.map do |pos|
+      y, x = pos
+      my_y, my_x = my_pos
+      [y + my_y, x + my_x]
+    end
   end
 
   def place_bomb(board, num = 9)
@@ -31,20 +88,5 @@ class Board
     bombed_board
   end
 
-  def render
-    puts
-    puts "  #{(0..8).to_a.join("  ")}"
-    board.each_with_index do |row, idx|
-      print "#{idx}"
-      row.each do |tile|
-        print " #{tile} "
-      end
-      puts
-    end
-    puts
-  end
-
-  private
-
-  attr_reader :board
+  
 end
--- tile.rb
+++ tile.rb
@@ -1,20 +1,9 @@
 class Tile
-  NEIGHBOR_POS = [
-    [-1, -1],
-    [-1, 0],
-    [-1, 1],
-    [0, -1],
-    [0, 1],
-    [1, -1],
-    [1, 0],
-    [1, 1]
-  ].freeze
-
   def initialize(bomb)
     @bomb = bomb
     @flagged = false
     @revealed = true
-    @fringe = 0
+    @neighbor_bombs = 0
   end
 
   def bomb?
@@ -43,33 +32,7 @@ class Tile
     end
   end
 
-  def neighbor_bomb_count(board)
-    my_pos = find_my_pos(board)
-    neighbor_idx = get_neighbor_idx(my_pos)
-    bomb_count = 0
-
-    neighbor_idx.each { |pos| bomb_count += 1 if board.get_tile(pos).bomb? }
-
-    @fringe = bomb_count
-  end
-
   private
 
   attr_reader :bomb, :flagged, :revealed, :fringe
-
-  def find_my_pos(board)
-    board.board.each_with_index do |row, y|
-      row.each_with_index do |tile, x|
-        return [y, x] if tile.object_id == self.object_id
-      end
-    end
-  end
-
-  def get_neighbor_idx(my_pos)
-    NEIGHBOR_POS.dup.map do |pos|
-      y, x = pos
-      my_y, my_x = my_pos
-      [y + my_y, x + my_x]
-    end
-  end
 end
