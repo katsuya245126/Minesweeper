a42e8cd7d825cb59a9f9e5ec166223a503f5bd87
Sung Guk Lee <katsuya245126@gmail.com>
2020-06-26 12:54:08 +0900

    Add methods to get input from the player

--- player.rb
+++ player.rb
@@ -1,2 +1,15 @@
 class Player
+  def prompt
+    prompt_message
+    input = gets.chomp
+    parse(input)
+  end
+
+  def parse(string)
+    string.split(',').map { |ele| Integer(ele) }
+  end
+
+  def prompt_message
+    print 'Input a coordinate(i.e 3,4): '
+  end
 end
