c7f51c6b87e459d51073e2a23fa22ff4819e16ab
Sung Guk Lee <katsuya245126@gmail.com>
2020-06-26 13:08:54 +0900

    Add run command with error catching

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,3 +1,45 @@
+require_relative 'board.rb'
+require_relative 'player.rb'
+
 class Minesweeper
-  def initialize end
+  def initialize
+    @board = Board.new
+    @player = Player.new
+  end
+
+  def run
+    until board.win?
+      refresh
+      begin
+        input = player.prompt
+      rescue ArgumentError
+        input_error
+        next
+      end
+
+      break
+    end
+  end
+
+  private
+
+  attr_reader :board, :player
+
+  def input_error
+    puts 'Invalid input! Make sure to enter a digit between 0-8 with a comma between.'
+    print 'Press Enter to continue...'
+    gets
+  end
+
+  def valid_pos?(pos)
+    pos.is_a?(Array) &&
+      pos.length == 2 &&
+      pos.all? { |i| i.between?(0, 8) } &&
+      !board.get_tile(pos).revealed?
+  end
+
+  def refresh
+    system('clear')
+    board.render
+  end
 end
